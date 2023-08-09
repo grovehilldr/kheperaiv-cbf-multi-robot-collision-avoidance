904afdffac856dbd35dc490c5f4ca7a411c965d5
grovehilldr <112022928+grovehilldr@users.noreply.github.com>
2023-08-09 17:28:45 -0400

    Update 4-Robot-Decentralized-SBC.py

--- Deadlock-Resolution/4-Robot-Decentralized-SBC.py
+++ Deadlock-Resolution/4-Robot-Decentralized-SBC.py
@@ -20,6 +20,8 @@ from utilities.pr_barrier_certs import *
 
 from rps.utilities.controllers import *
 from rps.utilities.barrier_certificates import *
+from rps.utilities.misc import *
+from rps.utilities.transformations import *
 start = time.time()
 
 rospy.init_node('Algorithm_PrSBC_Central', anonymous=True)
@@ -44,24 +46,26 @@ for i in range(khep_node_cnt):
 	pub.update({str(ip_num_list[i]) : rospy.Publisher('K4_controls_' + str(ip_num_list[i]), K4_controls, queue_size = 10)})
 
 # Robot dynamics variables
-uni_goals = np.array([[-0.9,-0.3,-0.3,-0.9  ], [0.9,0.9,0.3,0.3], [0,0,0,0]]) # changes based on Optitrack setup
+uni_goals = np.array([[1.0,1.0,-1.0,-1.0  ], [1.0,-1.0,-1.0,1.0], [0.1,0.1,0.1,0.1]]) # changes based on Optitrack setup
 Kp = 10 # TODO: might need to scale output depending on how large output of QP is 
 Vmax = 200
 Wmax = np.pi
 flag = np.zeros(khep_node_cnt) # stop flag
 
 # PrSBC Parameters
-safety_radius = 0.12
-barrier_gain = 1000
+safety_radius = 0.2
+barrier_gain = 1
 projection_distance = 0.05
 magnitude_limit = 0.2
-boundary_points = np.array([-1.15, -0.4, 0.5, 1.15])
-
-uni_controller = create_clf_unicycle_pose_controller()
-
-uni_barrier_cert = de_create_unicycle_barrier_certificate_with_boundary(barrier_gain=barrier_gain, safety_radius=safety_radius, projection_distance=projection_distance, magnitude_limit=magnitude_limit, boundary_points = boundary_points)
 
 
+single_integrator_position_controller = create_si_position_controller()
+
+si_barrier_cert = de_create_single_integrator_barrier_certificate()
+
+_, uni_to_si_states = create_si_to_uni_mapping()
+
+si_to_uni_dyn = create_si_to_uni_dynamics()
 '''
 v_rand_span = 0.005 * np.ones((2, n)) # setting up velocity error range for each robot
 x_rand_span_x = 0.02 * np.random.randint(3, 4, (1, n)) # setting up position error range for each robot,
@@ -69,36 +73,47 @@ x_rand_span_y = 0.02 * np.random.randint(1, 4, (1, n)) # rand_span serves as the
 x_rand_span_xy = np.concatenate((x_rand_span_x, x_rand_span_y))
 '''
 
-def control_robots(x0,x1,x2,x3, y0,y1,y2,y3, t0,t1,t2,t3 ,goals):
-	
+def control_robots(x0, x1, x2, x3, y0, y1, y2, y3, t0, t1, t2, t3, goals,p):
+	N =4
+	# reshape poses so that its 3 x n
+	x = np.array([[x0, x1, x2, x3], [y0, y1, y2, y3], [t0, t1, t2, t3]])
 
-    # reshape poses so that its 3 x n
-    x = np.array([[x0,x1,x2,x3], [y0,y1,y2,y3], [t0,t1,t2,t3]])
-    
 	# TODO: Add noise
 
-    # get distance to gaol for all robots
-    d = np.sqrt((goals[0][:4] - x[0][:4]) ** 2 + (goals[1][:4] - x[1][:4]) ** 2)
+	# get distance to gaol for all robots
+	d = np.sqrt((goals[0][:4] - x[0][:4]) ** 2 + (goals[1][:4] - x[1][:4]) ** 2)
 
-    # stop if distance threshold is met
-    if (d < .025).all() or (flag == 1).all():
-        V = np.zeros(khep_node_cnt)
-        W = np.zeros(khep_node_cnt)
-        sub.unregister()
-        return V, W
+	# stop if distance threshold is met
 
-    # Use a position controller to drive to the goal position
-    dxu = uni_controller(x, goals)
+	if (d < .075).all() or (flag == 1).all():
 
-    dxu = uni_barrier_cert(dxu, x)
+		V = 0
+		W = 0
+		sub.unregister()
+		return V, W
 
-    for i in range(khep_node_cnt):
-        if round(d[i], 2) < .05 or flag[i] == 1:
-            dxu[0, i] = 0
-            dxu[1, i] = 0
-            flag[i] = 1
 
-    return dxu[0] * 1000, dxu[1]
+	x = np.array([[x0, x1, x2, x3], [y0, y1, y2, y3], [t0, t1, t2, t3]])
+	x_si = uni_to_si_states(x)
+
+	# Create single-integrator control inputs
+	dxi = single_integrator_position_controller(x_si, uni_goals[:2][:])
+
+	# robot i
+	xx = np.reshape(x[:, p], (3, 1))
+	xi = np.reshape(x_si[:, p], (2, 1))
+	mask = np.arange(x_si.shape[1]) != p
+	xo = x_si[:, mask]  # for obstacles
+	dx = dxi[:, p]
+	dx = si_barrier_cert(dx, xi, xo)
+
+	du = si_to_uni_dyn(dx, xx)
+	dxu = np.zeros((2, N))
+	dxu[0, p] = du[0, 0]
+	dxu[1, p] = du[1, 0]
+
+
+	return dxu[0, p] * 500, dxu[1, p]/10.
 
 # This callback function is where the centralized swarm algorithm, or any algorithm should be
 # data is the info subscribed from the vicon node, contains the global position, velocity, etc
@@ -110,23 +125,24 @@ def callback(data):
 	# arrange positions and goals into PrSBC form
 	
 	# call control_robots with arranged positions/goals and return V and W list
-	
-	V, W = control_robots(data.x[0],data.x[1],data.x[2],data.x[3], data.y[0],data.y[1],data.y[2],data.y[3], data.theta[0],data.theta[1],data.theta[2],data.theta[3], uni_goals)
+	for p in range(4):
+		V, W = control_robots(data.x[0],data.x[1],data.x[2],data.x[3], data.y[0],data.y[1],data.y[2],data.y[3], data.theta[0],data.theta[1],data.theta[2],data.theta[3], uni_goals, p)
 
-	# send V and W list
-	for i in range(khep_node_cnt):
+
+		# send V and W list
+
 		# create control message
 		control_msgs = K4_controls()
-		
 		# set appropriate velocities
-		control_msgs.ctrl_V = V[i]
-		control_msgs.ctrl_W = W[i]
+		control_msgs.ctrl_V = V
+		control_msgs.ctrl_W = W
 
 		# send data
 		print('-------------------------')
-		print('Control for robot %s' % data.ip[i])
+		print('Control for robot %s' % data.ip[p])
 		print(control_msgs)
-		pub.get(str(data.ip[i])).publish(control_msgs)
+		pub.get(str(data.ip[p])).publish(control_msgs)
+
 
 def central():
 	global sub
